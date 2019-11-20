eacc7a5b53f0739b81dc21c07908bbce51bec1e2
jangseok.han <p084111@gmail.com>
2019-11-20 14:19:32 +0900

    add a transform

---
+++ Build/Source/System/Math/Primitive/Others/Transform.cpp
@@ -0,0 +1,32 @@
+﻿#include "Transform.h"
+
+namespace Engine5
+{
+    Transform::Transform()
+    {
+    }
+
+    Transform::~Transform()
+    {
+    }
+
+    Matrix44 Transform::LocalToWorldMatrix() const
+    {
+        return Matrix44();
+    }
+
+    Vector3 Transform::GetPosition() const
+    {
+        return m_position;
+    }
+
+    Vector3 Transform::GetScale() const
+    {
+        return m_scale;
+    }
+
+    Quaternion Transform::GetOrientation() const
+    {
+        return m_orientation;
+    }
+}
---
+++ Build/Source/System/Math/Structure/Transform.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "../Algebra/Vector3.hpp"
+#include "../Algebra/Quaternion.hpp"
+#include "../Algebra/Matrix44.hpp"
+
+namespace Engine5
+{
+    class Transform
+    {
+    public:
+        Transform();
+        ~Transform();
+
+        Matrix44 LocalToWorldMatrix() const;
+        Vector3 GetPosition() const;
+        Vector3 GetScale() const;
+        Quaternion GetOrientation() const;
+
+    private:
+        Vector3 m_position;
+        Vector3 m_scale;
+        Quaternion m_orientation;
+    };
+}
