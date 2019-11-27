908905b035dd0bc0bd54d887f9656600d974bc67
jangseok.han <p084111@gmail.com>
2019-11-27 11:47:52 +0900

    add a 2D Collider, and plane for geometry

---
+++ Build/Source/System/Physics/ColliderPrimitive/ConvexHull2D/ColliderTriangle.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "../ColliderPrimitive.hpp"
+
+namespace Engine5
+{
+    class ColliderTriangle final : public ColliderPrimitive
+    {
+    public:
+        ColliderTriangle();
+        ~ColliderTriangle() override;
+        ColliderTriangle(const ColliderTriangle& rhs) = delete;
+        ColliderTriangle& operator=(const ColliderTriangle& rhs) = delete;
+
+        void Initialize() override;
+        void Shutdown() override;
+
+        //minkowski support - gjk, epa
+        Vector3 Support(const Vector3& direction) override;
+
+        //ray - collider intersection
+        bool    TestRayIntersection(const Ray& local_ray, Real& minimum_t, Real& maximum_t) const override;
+        Vector3 GetNormal(const Vector3& local_point_on_collider) override;
+
+        //physics
+        void SetMassData(Real density) override;
+        Real GetVolume() override;
+
+        //collider 
+        void SetScaleData(const Vector3& scale) override;
+        void SetUnit() override;
+        void UpdateBoundingVolume() override;
+        void Draw(PrimitiveRenderer* renderer, RenderingMode mode, const Color& color) const override;
+
+    protected:
+        void Clone(ColliderPrimitive* cloned) override;
+
+    private:
+    };
+}
