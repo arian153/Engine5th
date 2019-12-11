8094de73f60522b8b2fa25c9055d5f556afd9298
jangseok.han <p084111@gmail.com>
2019-12-11 10:17:27 +0900

    add a tetrahedron normal vector calculation.

--- Build/Source/System/Physics/ColliderPrimitive/ConvexHull3D/ColliderTetrahedron.cpp
+++ Build/Source/System/Physics/ColliderPrimitive/ConvexHull3D/ColliderTetrahedron.cpp
@@ -91,7 +91,20 @@ namespace Engine5
 
     Vector3 ColliderTetrahedron::GetNormal(const Vector3& local_point_on_collider)
     {
-        return local_point_on_collider;
+        Vector3      normal;
+        ColliderFace faces[4];
+        faces[0] = ColliderFace(0, 1, 2);
+        faces[1] = ColliderFace(0, 1, 3);
+        faces[2] = ColliderFace(0, 2, 3);
+        faces[3] = ColliderFace(1, 2, 3);
+        for (auto& face : faces)
+        {
+            if (this->IsFaceContainPoint(face, local_point_on_collider, normal) == true)
+            {
+                return normal;
+            }
+        }
+        return normal;
     }
 
     void ColliderTetrahedron::SetMassData(Real density)
@@ -298,5 +311,33 @@ namespace Engine5
         }
         // intersect back side of ray.
         return false;
+    }
+
+    bool ColliderTetrahedron::IsFaceContainPoint(const ColliderFace& face, const Vector3& point, Vector3& normal) const
+    {
+        Vector3 v0     = Vertex(face.a);
+        Vector3 v1     = Vertex(face.b);
+        Vector3 v2     = Vertex(face.c);
+        Vector3 edge01 = v1 - v0;
+        Vector3 edge12 = v2 - v1;
+        normal         = edge01.CrossProduct(edge12);
+        Vector3 w_test = edge01.CrossProduct(point - v0);
+        if (w_test.DotProduct(normal) < 0.0f)
+        {
+            return false;
+        }
+        w_test = edge12.CrossProduct(point - v1);
+        if (w_test.DotProduct(normal) < 0.0f)
+        {
+            return false;
+        }
+        Vector3 edge3 = v0 - v2;
+        w_test        = edge3.CrossProduct(point - v2);
+        if (w_test.DotProduct(normal) < 0.0f)
+        {
+            return false;
+        }
+        normal.SetNormalize();
+        return true;
     }
 }
