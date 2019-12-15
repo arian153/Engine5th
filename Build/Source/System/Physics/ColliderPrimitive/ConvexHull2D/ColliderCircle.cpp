8946b58a3e2e4b116733117cf572f98386b4fc38
jangseok.han <p084111@gmail.com>
2019-12-15 21:55:35 +0900

    add a circle draw code.

--- Build/Source/System/Physics/ColliderPrimitive/ConvexHull2D/ColliderCircle.cpp
+++ Build/Source/System/Physics/ColliderPrimitive/ConvexHull2D/ColliderCircle.cpp
@@ -72,21 +72,82 @@ namespace Engine5
 
     void ColliderCircle::SetScaleData(const Vector3& scale)
     {
+        m_scaled_radius = m_radius * scale.Length();
+        m_scale_factor = scale.Length();
     }
 
     void ColliderCircle::SetUnit()
     {
         m_radius = 0.5f;
+        UpdatePrimitive();
     }
 
     void ColliderCircle::UpdateBoundingVolume()
     {
-        //todo temporary code
-        m_bounding_volume->Set(Vector3::Origin(), Vector3::Origin());
+        Real    bounding_factor = m_radius;
+        Vector3 pos;
+        if (m_rigid_body != nullptr)
+        {
+            pos = m_rigid_body->LocalToWorldPoint(m_position);
+            bounding_factor *= m_scale_factor;
+        }
+        else
+        {
+            pos = m_position;
+        }
+        Vector3 min_max(bounding_factor, bounding_factor, bounding_factor);
+        m_bounding_volume->Set(-min_max + pos, min_max + pos);
     }
 
     void ColliderCircle::Draw(PrimitiveRenderer* renderer, RenderingMode mode, const Color& color) const
     {
+        I32 index = static_cast<I32>(renderer->VerticesSize(mode));
+        I32 count = renderer->CIRCULAR_VERTICES_COUNT;
+        renderer->ReserveVertices(count, mode);
+        Vector3    body_position = GetBodyPosition();
+        Quaternion body_orientation = GetBodyOrientation();
+        Real    radius = Radius();
+        Real       radian_step = Math::TWO_PI / static_cast<Real>(count);
+        for (int i = 0; i < count; ++i)
+        {
+            Real    angle = static_cast<Real>(i) * radian_step;
+            Vector3 vertex(cosf(angle) * radius, sinf(angle) * radius, 0.0f);
+            vertex = m_orientation.Rotate(vertex);
+            vertex += m_position;
+            vertex = body_orientation.Rotate(vertex);
+            vertex += body_position;
+            renderer->PushVertex(vertex, mode, color);
+        }
+        if (mode == RenderingMode::Dot)
+        {
+            for (I32 i = 0; i < count; ++i)
+            {
+                renderer->PushIndex(index + i, mode);
+            }
+        }
+        else if (mode == RenderingMode::Line)
+        {
+            renderer->ReserveIndices(200, mode);
+            for (int i = 0; i < count - 1; ++i)
+            {
+                renderer->PushLineIndices(index + i, index + i + 1);
+            }
+            renderer->PushLineIndices(index + count - 1, index);
+        }
+        else if (mode == RenderingMode::Face)
+        {
+            //add a center pos
+            I32     center = static_cast<I32>(renderer->VerticesSize(mode));
+            Vector3 position = m_position;
+            position = body_orientation.Rotate(position);
+            position += body_position;
+            renderer->PushVertex(position, mode, color);
+            for (int i = 0; i < count - 1; ++i)
+            {
+                renderer->PushFaceIndices(center, index + i + 1, index + i);
+            }
+            renderer->PushFaceIndices(center, index, index + count - 1);
+        }
     }
 
     Real ColliderCircle::Radius() const
