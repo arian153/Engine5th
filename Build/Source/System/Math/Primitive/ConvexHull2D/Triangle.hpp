2fe13400338503b53f7751eca23668a92f1235d5
jangseok.han <p084111@gmail.com>
2019-12-23 11:15:16 +0900

    add a missing code.

--- Build/Source/System/Math/Primitive/ConvexHull2D/Triangle.hpp
+++ Build/Source/System/Math/Primitive/ConvexHull2D/Triangle.hpp
@@ -37,7 +37,6 @@ namespace Engine5
         Real DistanceSquared(const Vector3& point) const;
 
     public:
-        static Real DistanceSquared(const Vector3& point, const Vector3& v0, const Vector3& v1, const Vector3& v2);
 
 
     public:
