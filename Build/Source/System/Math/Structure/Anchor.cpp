dbefe22be0acde27b3aa4d758d33d75ec1f8a61b
jangseok.han <p084111@gmail.com>
2019-12-30 10:55:53 +0900

    fix a anchor code.

--- Build/Source/System/Math/Structure/Anchor.cpp
+++ Build/Source/System/Math/Structure/Anchor.cpp
@@ -1,4 +1,5 @@
 #include "Anchor.hpp"
+#include "../Utility/VectorDef.hpp"
 
 namespace Engine5
 {
@@ -10,204 +11,109 @@ namespace Engine5
     {
     }
 
-    Anchor2D Anchor::ToAnchor2D(AnchorDirection horizontal, AnchorDirection vertical)
+    Anchor2D Anchor::ToAnchor2D(AnchorLR left_right, AnchorUD up_down)
     {
-        if (horizontal == AnchorDirection::Left)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                return Anchor2D::TL;
-            }
-            if (vertical == AnchorDirection::Center)
-            {
-                return Anchor2D::CL;
-            }
-            if (vertical == AnchorDirection::Bottom)
-            {
-                return Anchor2D::BL;
-            }
-        }
-        else if (horizontal == AnchorDirection::Center)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                return Anchor2D::TC;
-            }
-            if (vertical == AnchorDirection::Center)
-            {
-                return Anchor2D::CC;
-            }
-            if (vertical == AnchorDirection::Bottom)
-            {
-                return Anchor2D::BC;
-            }
-        }
-        else if (horizontal == AnchorDirection::Right)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                return Anchor2D::TR;
-            }
-            if (vertical == AnchorDirection::Center)
-            {
-                return Anchor2D::CR;
-            }
-            if (vertical == AnchorDirection::Bottom)
-            {
-                return Anchor2D::BR;
-            }
-        }
-
-        return Anchor2D::Invalid;
+        return static_cast<Anchor2D>(static_cast<size_t>(left_right) + static_cast<size_t>(up_down));
     }
 
-    Anchor3D Anchor::ToAnchor3D(AnchorDirection horizontal, AnchorDirection vertical, AnchorDirection front_rear)
+    Anchor3D Anchor::ToAnchor3D(AnchorLR left_right, AnchorUD up_down, AnchorFB front_back)
     {
-        if (horizontal == AnchorDirection::Left)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::TLF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::TLC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::TLB;
-                }
-            }
-            else if (vertical == AnchorDirection::Center)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::CLF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::CLC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::CLB;
-                }
-            }
-            else if (vertical == AnchorDirection::Bottom)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::BLF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::BLC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::BLB;
-                }
-            }
-        }
-        else if (horizontal == AnchorDirection::Center)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::TCF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::TCC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::TCB;
-                }
-            }
-            else if (vertical == AnchorDirection::Center)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::CCF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::CCC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::CCB;
-                }
-            }
-            else if (vertical == AnchorDirection::Bottom)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::BCF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::BCC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::BCB;
-                }
-            }
-        }
-        else if (horizontal == AnchorDirection::Right)
-        {
-            if (vertical == AnchorDirection::Top)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::TRF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::TRC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::TRB;
-                }
-            }
-            else if (vertical == AnchorDirection::Center)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::CRF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::CRC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::CRB;
-                }
-            }
-            else if (vertical == AnchorDirection::Bottom)
-            {
-                if (front_rear == AnchorDirection::Front)
-                {
-                    return Anchor3D::BRF;
-                }
-                if (front_rear == AnchorDirection::Center)
-                {
-                    return Anchor3D::BRC;
-                }
-                if (front_rear == AnchorDirection::Back)
-                {
-                    return Anchor3D::BRB;
-                }
-            }
-        }
+        return static_cast<Anchor3D>(static_cast<size_t>(left_right) + static_cast<size_t>(up_down) + static_cast<size_t>(front_back));
+    }
 
-        return Anchor3D::Invalid;
+    Vector2 Anchor::AnchorVector2(Anchor2D anchor)
+    {
+        size_t anchor_code = static_cast<size_t>(anchor);
+        size_t x           = anchor_code / 100;
+        size_t yz          = anchor_code % 100;
+        size_t y           = yz / 10;
+        return ToVector2(static_cast<AnchorLR>(x * 100), static_cast<AnchorUD>(y * 10));
+    }
+
+    Vector3 Anchor::AnchorVector3(Anchor3D anchor)
+    {
+        size_t anchor_code = static_cast<size_t>(anchor);
+        size_t x           = anchor_code / 100;
+        size_t yz          = anchor_code % 100;
+        size_t y           = yz / 10;
+        size_t z           = yz % 10;
+        return ToVector3(static_cast<AnchorLR>(x * 100), static_cast<AnchorUD>(y * 10), static_cast<AnchorFB>(z));
+    }
+
+    Vector2 Anchor::ToVector2(AnchorLR left_right, AnchorUD up_down)
+    {
+        Vector2 result;
+        switch (left_right)
+        {
+        case Engine5::AnchorLR::Left:
+            result -= Math::Vector2::X_AXIS;
+            break;
+        case Engine5::AnchorLR::Center:
+            break;
+        case Engine5::AnchorLR::Right:
+            result += Math::Vector2::X_AXIS;
+            break;
+        default:
+            break;
+        }
+        switch (up_down)
+        {
+        case Engine5::AnchorUD::Up:
+            result += Math::Vector2::Y_AXIS;
+            break;
+        case Engine5::AnchorUD::Center:
+            break;
+        case Engine5::AnchorUD::Down:
+            result -= Math::Vector2::Y_AXIS;
+            break;
+        default:
+            break;
+        }
+        return result;
+    }
+
+    Vector3 Anchor::ToVector3(AnchorLR left_right, AnchorUD up_down, AnchorFB front_back)
+    {
+        Vector3 result;
+        switch (left_right)
+        {
+        case Engine5::AnchorLR::Left:
+            result -= Math::Vector3::X_AXIS;
+            break;
+        case Engine5::AnchorLR::Center:
+            break;
+        case Engine5::AnchorLR::Right:
+            result += Math::Vector3::X_AXIS;
+            break;
+        default:
+            break;
+        }
+        switch (up_down)
+        {
+        case Engine5::AnchorUD::Up:
+            result += Math::Vector3::Y_AXIS;
+            break;
+        case Engine5::AnchorUD::Center:
+            break;
+        case Engine5::AnchorUD::Down:
+            result -= Math::Vector3::Y_AXIS;
+            break;
+        default:
+            break;
+        }
+        switch (front_back)
+        {
+        case Engine5::AnchorFB::Front:
+            result += Math::Vector3::Z_AXIS;
+            break;
+        case Engine5::AnchorFB::Center:
+            break;
+        case Engine5::AnchorFB::Back:
+            result -= Math::Vector3::Z_AXIS;
+            break;
+        default:
+            break;
+        }
+        return result;
     }
 }
--- Build/Source/System/Math/Structure/Anchor.hpp
+++ Build/Source/System/Math/Structure/Anchor.hpp
@@ -1,38 +1,48 @@
 #pragma once
+#include "../Algebra/Vector2.hpp"
+#include "../Algebra/Vector3.hpp"
 
 namespace Engine5
 {
-    enum class Anchor2D
+    //Left-Right, Up-Down
+    enum class Anchor2D : size_t
     {
-        TL, TC, TR,
-        CL, CC, CR,
-        BL, BC, BR,
-        Invalid
+        LU = 110, CU = 210, RU = 310,
+        LC = 120, CC = 220, RC = 320,
+        LD = 130, CD = 230, RD = 330,
+        Invalid = 000
     };
 
-    //Top-Bottom, Left-Right, Front-Back
-    enum class Anchor3D
+    //Left-Right, Up-Down, Front-Back
+    enum class Anchor3D : size_t
     {
-        TLF, TLC, TLB,
-        TCF, TCC, TCB,
-        TRF, TRC, TRB,
+        LUF = 111, CUF = 211, RUF = 311,
+        LUC = 112, CUC = 212, RUC = 312,
+        LUB = 113, CUB = 213, RUB = 313,
 
-        CLF, CLC, CLB,
-        CCF, CCC, CCB,
-        CRF, CRC, CRB,
+        LCF = 121, CCF = 221, RCF = 321,
+        LCC = 122, CCC = 222, RCC = 322,
+        LCB = 123, CCB = 223, RCB = 323,
 
-        BLF, BLC, BLB,
-        BCF, BCC, BCB,
-        BRF, BRC, BRB,
-        Invalid
+        LDF = 131, CDF = 231, RDF = 331,
+        LDC = 132, CDC = 232, RDC = 332,
+        LDB = 133, CDB = 233, RDB = 333,
+        Invalid = 000
     };
 
-    enum class AnchorDirection
+    enum class AnchorLR : size_t
     {
-        Top, Bottom,
-        Left, Right,
-        Front, Back,
-        Center, Invalid
+        Left = 100, Center = 200, Right = 300
+    };
+
+    enum class AnchorUD : size_t
+    {
+        Up = 10, Center = 20, Down = 30
+    };
+
+    enum class AnchorFB : size_t
+    {
+        Front = 1, Center = 2, Back = 3
     };
 
     class Anchor
@@ -41,8 +51,14 @@ namespace Engine5
         Anchor();
         ~Anchor();
 
-        static Anchor2D ToAnchor2D(AnchorDirection horizontal, AnchorDirection vertical);
-        static Anchor3D ToAnchor3D(AnchorDirection horizontal, AnchorDirection vertical, AnchorDirection front_rear);
+        static Anchor2D ToAnchor2D(AnchorLR left_right, AnchorUD up_down);
+        static Anchor3D ToAnchor3D(AnchorLR left_right, AnchorUD up_down, AnchorFB front_back);
+        static Vector2 AnchorVector2(Anchor2D anchor);
+        static Vector3 AnchorVector3(Anchor3D anchor);
+
+        static Vector2 ToVector2(AnchorLR left_right, AnchorUD up_down);
+        static Vector3 ToVector3(AnchorLR left_right, AnchorUD up_down, AnchorFB front_back);
+        
     private:
 
     };
