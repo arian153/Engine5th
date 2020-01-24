832e9865370407560c30b46957763a7e20fb6353
jangseok.han (arian153) <p084111@gmail.com>
2020-01-24 16:09:07 +0900

    add a random code from prev proj

--- Build/Source/System/Math/Utility/Random.cpp
+++ Build/Source/System/Math/Utility/Random.cpp
@@ -1,4 +1,162 @@
+#include "Random.hpp"
+
 namespace Engine5
 {
+    Random::Random()
+    {
+        GenerateSeed();
+    }
+
+    Random::Random(U32 seed)
+    {
+        m_seed = seed;
+        m_random32 = std::mt19937(seed);
+        m_random64 = std::mt19937_64(seed);
+    }
+
+    Random::~Random()
+    {
+    }
+
+    void Random::SetSeed(U32 seed)
+    {
+        m_seed = seed;
+        m_random32 = std::mt19937(seed);
+        m_random64 = std::mt19937_64(seed);
+    }
+
+    U32 Random::GetSeed() const
+    {
+        return m_seed;
+    }
+
+    U32 Random::GenerateSeed()
+    {
+        m_seed = m_rng();
+        m_random32 = std::mt19937(m_seed);
+        m_random64 = std::mt19937_64(m_seed);
+
+        return m_seed;
+    }
+
+    U32 Random::GetRandomU32()
+    {
+        return m_random32();
+    }
+
+    U64 Random::GetRandomU64()
+    {
+        return m_random64();
+    }
+
+    I32 Random::GetRandomI32()
+    {
+        return static_cast<I32>(m_random32());
+    }
+
+    I64 Random::GetRandomI64()
+    {
+        return static_cast<I64>(m_random64());
+    }
+
+    R64 Random::GetRandomR64()
+    {
+        return static_cast<R64>(m_random64()) / static_cast<R64>(m_random64.max());
+    }
+
+    Real Random::GetRandomReal()
+    {
+        return static_cast<Real>(m_random32()) / static_cast<Real>(m_random32.max());
+    }
+
+    U32 Random::GetRangedRandomU32(U32 min, U32 max)
+    {
+        std::uniform_int_distribution<U32> dist(min, max);
+        return dist(m_random32);
+    }
+
+    U64 Random::GetRangedRandomU64(U64 min, U64 max)
+    {
+        std::uniform_int_distribution<U64> dist(min, max);
+        return dist(m_random64);
+    }
+
+    I32 Random::GetRangedRandomI32(I32 min, I32 max)
+    {
+        std::uniform_int_distribution<I32> dist(min, max);
+        return dist(m_random32);
+    }
+
+    I64 Random::GetRangedRandomI64(I64 min, I64 max)
+    {
+        std::uniform_int_distribution<I64> dist(min, max);
+        return dist(m_random64);
+    }
+
+    R64 Random::GetRangedRandomR64(R64 min, R64 max)
+    {
+        std::uniform_real_distribution<R64> dist(min, max);
+        return dist(m_random64);
+    }
+
+    Real Random::GetRangedRandomReal(Real min, Real max)
+    {
+        std::uniform_real_distribution<Real> dist(min, max);
+        return dist(m_random32);
+    }
+
+    bool Random::IsNPercentSuccessReal(Real percent)
+    {
+        std::bernoulli_distribution dist(percent);
+        return dist(m_random32);
+            }
+
+    bool Random::IsNPercentSuccessR64(R64 percent)
+    {
+        std::bernoulli_distribution dist(percent);
+        return dist(m_random64);
+    }
+
+    I32 Random::NPercentSuccessMTimesResult(I32 m, Real percent)
+    {
+        std::binomial_distribution<> dist(m, percent);
+        return dist(m_random32);
+    }
+
+    U32 Random::GetNormalDistributionU32(U32 average, U32 deviation)
+    {
+        std::normal_distribution<> dist(average, deviation);
+        return static_cast<U32>(dist(m_random32));
+    }
+
+    U64 Random::GetNormalDistributionU64(U64 average, U64 deviation)
+    {
+        std::normal_distribution<> dist(static_cast<R64>(average), static_cast<R64>(deviation));
+        return static_cast<U64>(dist(m_random64));
+    }
+
+    I32 Random::GetNormalDistributionI32(I32 average, I32 deviation)
+    {
+        std::normal_distribution<> dist(average, deviation);
+        return static_cast<I32>(dist(m_random32));
+    }
+
+    I64 Random::GetNormalDistributionI64(I64 average, I64 deviation)
+    {
+        std::normal_distribution<> dist(static_cast<R64>(average), static_cast<R64>(deviation));
+        return static_cast<I64>(dist(m_random64));
+    }
+
+    R64 Random::GetNormalDistributionR64(R64 average, R64 deviation)
+    {
+        std::normal_distribution<> dist(average, deviation);
+        return dist(m_random64);
+    }
+
+    Real Random::GetNormalDistributionReal(Real average, Real deviation)
+    {
+        std::normal_distribution<> dist(average, deviation);
+        return static_cast<Real>(dist(m_random32));
+    }
 
 }
--- Build/Source/System/Math/Utility/Random.hpp
+++ Build/Source/System/Math/Utility/Random.hpp
@@ -1,6 +1,52 @@
 #pragma once
+#include <random>
+#include "../../Core/Utility/CoreDef.hpp"
+#include "MathDef.hpp"
 
 namespace Engine5
 {
+    class Random
+    {
+    public:
+        Random();
+        Random(U32 seed);
+        ~Random();
+
+        void SetSeed(U32 seed);
+        U32 GetSeed() const;
+        U32 GenerateSeed();
+
+        U32 GetRandomU32();
+        U64 GetRandomU64();
+        I32 GetRandomI32();
+        I64 GetRandomI64();
+        R64 GetRandomR64();
+
+        Real GetRandomReal();
+
+        U32 GetRangedRandomU32(U32 min, U32 max);
+        U64 GetRangedRandomU64(U64 min, U64 max);
+        I32 GetRangedRandomI32(I32 min, I32 max);
+        I64 GetRangedRandomI64(I64 min, I64 max);
+        R64 GetRangedRandomR64(R64 min, R64 max);
+        Real GetRangedRandomReal(Real min, Real max);
+
+        bool IsNPercentSuccessReal(Real percent);
+        bool IsNPercentSuccessR64(R64 percent);
+
+        I32 NPercentSuccessMTimesResult(I32 m, Real percent);
+        U32 GetNormalDistributionU32(U32 average, U32 deviation);
+        U64 GetNormalDistributionU64(U64 average, U64 deviation);
+        I32 GetNormalDistributionI32(I32 average, I32 deviation);
+        I64 GetNormalDistributionI64(I64 average, I64 deviation);
+        R64 GetNormalDistributionR64(R64 average, R64 deviation);
+        Real GetNormalDistributionReal(Real average, Real deviation);
+
+    private:
+        U32 m_seed;
+        std::mt19937 m_random32;
+        std::mt19937_64 m_random64;
+        std::random_device m_rng;
+    };
 
 }
