dd95c25155f5c04a8752cf2226fa5d85fb7d0676
jangseok.han (arian153) <p084111@gmail.com>
2020-02-11 10:00:38 +0900

    add a game object code.

--- Build/Source/Manager/Object/Object.cpp
+++ Build/Source/Manager/Object/Object.cpp
@@ -3,10 +3,39 @@
 namespace Engine5
 {
     Object::Object()
+        : m_id(s_id++)
     {
     }
 
     Object::~Object()
     {
+    }
+
+    Object* Object::GetChildAt(size_t index) const
+    {
+        if (m_children != nullptr)
+        {
+            return m_children->at(index);
+        }
+        return nullptr;
+    }
+
+    Object* Object::GetSiblingAt(size_t index) const
+    {
+        if (m_sibling != nullptr)
+        {
+            return m_sibling->at(index);
+        }
+        return nullptr;
+    }
+
+    Object* Object::GetParent() const
+    {
+        return m_parent;
+    }
+
+    Object* Object::GetAncestor() const
+    {
+        return m_ancestor;
     }
 }
--- Build/Source/Manager/Object/Object.hpp
+++ Build/Source/Manager/Object/Object.hpp
@@ -1,14 +1,38 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace Engine5
 {
+    class Component;
+
     class Object
     {
     public:
         Object();
         ~Object();
 
+        Object* GetChildAt(size_t index) const;
+        Object* GetSiblingAt(size_t index) const;
+        Object* GetParent() const;
+        Object* GetAncestor() const;
+
     private:
+        inline static size_t s_id = 0;
+
+    private:
+        size_t      m_id;
+        std::string m_name;
+
+        //family
+        Object*               m_ancestor = nullptr;
+        Object*               m_parent   = nullptr;
+        std::vector<Object*>* m_children = nullptr;
+        std::vector<Object*>* m_sibling  = nullptr;
+
+        //components
+        std::unordered_map<std::string, Component*> m_components;
     };
 }
 
