4b0981faab980f4b58b564a8d2cc3c5fe03bc136
jangseok.han (arian153) <p084111@gmail.com>
2020-03-13 22:05:56 +0900

    add a prototype of json resource

--- Build/Source/Manager/Resource/ResourceType/JsonResource.cpp
+++ Build/Source/Manager/Resource/ResourceType/JsonResource.cpp
@@ -11,4 +11,44 @@ namespace Engine5
     JsonResource::~JsonResource()
     {
     }
+
+    bool JsonResource::IsLevel() const
+    {
+        return m_json_type == eJsonType::Level;
+    }
+
+    bool JsonResource::IsSpace() const
+    {
+        return m_json_type == eJsonType::Space;
+    }
+
+    bool JsonResource::IsArchetype() const
+    {
+        return m_json_type == eJsonType::Archetype;
+    }
+
+    bool JsonResource::IsObject() const
+    {
+        return m_json_type == eJsonType::Object;
+    }
+
+    bool JsonResource::IsMeshData() const
+    {
+        return m_json_type == eJsonType::MeshData;
+    }
+
+    bool JsonResource::IsParticleData() const
+    {
+        return m_json_type == eJsonType::ParticleData;
+    }
+
+    eJsonType JsonResource::GetType() const
+    {
+        return m_json_type;
+    }
+
+    void JsonResource::LoadType()
+    {
+
+    }
 }
--- Build/Source/Manager/Resource/ResourceType/JsonResource.hpp
+++ Build/Source/Manager/Resource/ResourceType/JsonResource.hpp
@@ -3,14 +3,34 @@
 
 namespace Engine5
 {
+    enum class eJsonType : size_t
+    {
+        None
+      , Level
+      , Space
+      , Archetype
+      , Object
+      , MeshData
+      , ParticleData
+    };
+
     class JsonResource final : public Resource
     {
     public:
         explicit JsonResource(const std::wstring& path);
         ~JsonResource();
 
+        bool      IsLevel() const;
+        bool      IsSpace() const;
+        bool      IsArchetype() const;
+        bool      IsObject() const;
+        bool      IsMeshData() const;
+        bool      IsParticleData() const;
+        eJsonType GetType() const;
+
+        void LoadType();
+
     private:
-
-
+        eJsonType m_json_type = eJsonType::None;
     };
 }
