734cabf96fe284d9e9e838e44618f26ec9f77e43
jangseok.han (arian153) <p084111@gmail.com>
2020-04-08 13:14:55 +0900

    add a code.

--- Build/Source/System/Graphics/API/DX11/Shader/DeferredShaderDX11.cpp
+++ Build/Source/System/Graphics/API/DX11/Shader/DeferredShaderDX11.cpp
@@ -2,5 +2,26 @@
 
 namespace Engine5
 {
+    DeferredShaderDX11::DeferredShaderDX11()
+    {
+    }
 
+    DeferredShaderDX11::~DeferredShaderDX11()
+    {
+    }
+
+    void DeferredShaderDX11::SetHWnd(HWND hwnd)
+    {
+        m_hwnd = hwnd;
+    }
+
+    void DeferredShaderDX11::SetDevice(ID3D11Device* device)
+    {
+        m_device = device;
+    }
+
+    void DeferredShaderDX11::SetDeviceContext(ID3D11DeviceContext* device_context)
+    {
+        m_device_context = device_context;
+    }
 }
