f1cebf074d5446151ff1bcabeb8e727b1a66eff9
jangseok.han (arian153) <p084111@gmail.com>
2020-04-30 16:33:30 +0900

    add a code

--- Build/Source/System/Graphics/API/DX11/Buffer/TextObjectBufferDX11.cpp
+++ Build/Source/System/Graphics/API/DX11/Buffer/TextObjectBufferDX11.cpp
@@ -11,6 +11,54 @@ namespace Engine5
     {
     }
 
+    bool TextObjectBufferDX11::SetUpTextFormats()
+    {
+        HRESULT result = m_write_factory->CreateTextFormat(
+                                                           L"courier new",
+                                                           nullptr,
+                                                           DWRITE_FONT_WEIGHT_NORMAL,
+                                                           DWRITE_FONT_STYLE_NORMAL,
+                                                           DWRITE_FONT_STRETCH_NORMAL,
+                                                           14.0f,
+                                                           L"en-GB",
+                                                           &m_text_format
+                                                          );
+        if (FAILED(result))
+        {
+            return false;
+        }
+        result = m_text_format->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_LEADING);
+        if (FAILED(result))
+        {
+            return false;
+        }
+        result = m_text_format->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR);
+        if (FAILED(result))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    bool TextObjectBufferDX11::SetUpTextLayout()
+    {
+
+        HRESULT result = m_write_factory->CreateTextLayout(
+            text.c_str(),      // The string to be laid out and formatted.
+            (UINT32)text.length(),  // The length of the string.
+            m_text_format,  // The text format to apply to the string (contains font information, etc).
+            (Real)width,         // The width of the layout box.
+            (Real)height,        // The height of the layout box.
+            &m_text_layout  // The IDWriteTextLayout interface pointer.
+        );
+
+        if (FAILED(result))
+        {
+            return false;
+        }
+        return true;
+    }
+
     TextObjectBufferCommon::TextObjectBufferCommon()
     {
     }
--- Build/Source/System/Graphics/API/DX11/Buffer/TextObjectBufferDX11.hpp
+++ Build/Source/System/Graphics/API/DX11/Buffer/TextObjectBufferDX11.hpp
@@ -1,4 +1,10 @@
 #pragma once
+#include <d3d11.h>
+#include <dwrite_2.h>
+#include <d2d1_2.h>
+#include <string>
+#include "../../../../Math/Algebra/Vector2.hpp"
+#include "../../../DataType/Color.hpp"
 
 namespace Engine5
 {
@@ -8,6 +14,17 @@ namespace Engine5
         TextObjectBufferDX11();
         ~TextObjectBufferDX11();
 
+        bool SetUpTextFormats();
+        bool SetUpTextLayout();
+
     private:
+        IDWriteFactory2* m_write_factory = nullptr;
+        IDWriteTextFormat*    m_text_format = nullptr;
+        IDWriteTextLayout*    m_text_layout = nullptr;
+        ID2D1SolidColorBrush* m_brush       = nullptr;
+        Vector2               m_position;
+        Color                 m_color;
+
+        
     };
 }
