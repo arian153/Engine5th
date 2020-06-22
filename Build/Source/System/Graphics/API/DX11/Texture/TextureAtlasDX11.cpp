2abbb4556b9351077730d94d91285b0f657deb87
jangseok.han (arian153) <p084111@gmail.com>
2020-06-22 17:27:08 +0900

    add a method about converting UV coordinate to texture atlas coordinate

--- Build/Source/System/Graphics/API/DX11/Texture/TextureAtlasDX11.cpp
+++ Build/Source/System/Graphics/API/DX11/Texture/TextureAtlasDX11.cpp
@@ -18,4 +18,15 @@ namespace Engine5
     TextureAtlasCommon::~TextureAtlasCommon()
     {
     }
+
+    Vector4 TextureAtlasCommon::GetUVRect(size_t index)
+    {
+        return m_fragments.at(index);
+    }
+
+    Vector2 TextureAtlasCommon::ConvertUV(const Vector2& uv, size_t index)
+    {
+        Vector4 rect = m_fragments.at(index);
+        return Vector2(rect.x + uv.x * (rect.z - rect.x), rect.y + uv.y * (rect.w - rect.y));
+    }
 }
--- Build/Source/System/Graphics/Texture/TextureAtlasCommon.hpp
+++ Build/Source/System/Graphics/Texture/TextureAtlasCommon.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include "../API/GraphicsAPI.hpp"
 #include IncludeTextureAtlasAPI
+#include "../../Math/Algebra/Vector4.hpp"
+#include "../../Math/Algebra/Vector2.hpp"
+#include <vector>
 
 namespace Engine5
 {
@@ -10,6 +13,11 @@ namespace Engine5
         TextureAtlasCommon();
         ~TextureAtlasCommon();
 
+        Vector4 GetUVRect(size_t index);
+
+        Vector2 ConvertUV(const Vector2& uv, size_t index);
+
     private:
+        std::vector<Vector4> m_fragments;
     };
 }
