2bc664b173c6438aafe621c0390243046ac704b8
jangseok.han (arian153) <p084111@gmail.com>
2020-07-27 23:45:14 +0900

    Add a black board code. these data will shared in action list and behavior tree.

--- Build/Source/System/Logic/ArtificialIntelligence/BlackBoard.cpp
+++ Build/Source/System/Logic/ArtificialIntelligence/BlackBoard.cpp
@@ -9,4 +9,222 @@ namespace Engine5
     BlackBoard::~BlackBoard()
     {
     }
+
+    bool BlackBoard::HasKey(const std::string& key)
+    {
+        return m_booleans.find(key) != m_booleans.end() ||
+                m_integers.find(key) != m_integers.end() ||
+                m_positives.find(key) != m_positives.end() ||
+                m_reals.find(key) != m_reals.end() ||
+                m_vectors.find(key) != m_vectors.end() ||
+                m_quaternions.find(key) != m_quaternions.end() ||
+                m_strings.find(key) != m_strings.end();
+    }
+
+    void BlackBoard::Clear()
+    {
+        m_booleans.clear();
+        m_integers.clear();
+        m_positives.clear();
+        m_reals.clear();
+        m_vectors.clear();
+        m_quaternions.clear();
+        m_strings.clear();
+    }
+
+    void BlackBoard::SetBoolean(const std::string& key, bool data)
+    {
+        m_booleans[key] = data;
+    }
+
+    void BlackBoard::SetInteger(const std::string& key, int data)
+    {
+        m_integers[key] = data;
+    }
+
+    void BlackBoard::SetPositive(const std::string& key, size_t data)
+    {
+        m_positives[key] = data;
+    }
+
+    void BlackBoard::SetReal(const std::string& key, Real data)
+    {
+        m_reals[key] = data;
+    }
+
+    void BlackBoard::SetVector(const std::string& key, const Vector3& data)
+    {
+        m_vectors[key] = data;
+    }
+
+    void BlackBoard::SetQuaternion(const std::string& key, const Quaternion& data)
+    {
+        m_quaternions[key] = data;
+    }
+
+    void BlackBoard::SetString(const std::string& key, const std::string& data)
+    {
+        m_strings[key] = data;
+    }
+
+    bool BlackBoard::GetBoolean(const std::string& key)
+    {
+        if (m_booleans.find(key) == m_booleans.end())
+        {
+            m_booleans.emplace(key, false);
+        }
+        return m_booleans[key];
+    }
+
+    int BlackBoard::GetInteger(const std::string& key)
+    {
+        if (m_integers.find(key) == m_integers.end())
+        {
+            m_integers.emplace(key, 0);
+        }
+        return m_integers[key];
+    }
+
+    size_t BlackBoard::GetPositive(const std::string& key)
+    {
+        if (m_positives.find(key) == m_positives.end())
+        {
+            m_positives.emplace(key, 0);
+        }
+        return m_positives[key];
+    }
+
+    Real BlackBoard::GetReal(const std::string& key)
+    {
+        if (m_reals.find(key) == m_reals.end())
+        {
+            m_reals.emplace(key, 0.0f);
+        }
+        return m_reals[key];
+    }
+
+    Vector3 BlackBoard::GetVector(const std::string& key)
+    {
+        if (m_vectors.find(key) == m_vectors.end())
+        {
+            m_vectors.emplace(key, Vector3());
+        }
+        return m_vectors[key];
+    }
+
+    Quaternion BlackBoard::GetQuaternion(const std::string& key)
+    {
+        if (m_quaternions.find(key) == m_quaternions.end())
+        {
+            m_quaternions.emplace(key, Quaternion());
+        }
+        return m_quaternions[key];
+    }
+
+    std::string BlackBoard::GetString(const std::string& key)
+    {
+        if (m_strings.find(key) == m_strings.end())
+        {
+            m_strings.emplace(key, "");
+        }
+        return m_strings[key];
+    }
+
+    bool& BlackBoard::GetReferencedBoolean(const std::string& key)
+    {
+        if (m_booleans.find(key) == m_booleans.end())
+        {
+            m_booleans.emplace(key, false);
+        }
+        return m_booleans[key];
+    }
+
+    int& BlackBoard::GetReferencedInteger(const std::string& key)
+    {
+        if (m_integers.find(key) == m_integers.end())
+        {
+            m_integers.emplace(key, 0);
+        }
+        return m_integers[key];
+    }
+
+    size_t& BlackBoard::GetReferencedPositive(const std::string& key)
+    {
+        if (m_positives.find(key) == m_positives.end())
+        {
+            m_positives.emplace(key, 0);
+        }
+        return m_positives[key];
+    }
+
+    Real& BlackBoard::GetReferencedReal(const std::string& key)
+    {
+        if (m_reals.find(key) == m_reals.end())
+        {
+            m_reals.emplace(key, 0.0f);
+        }
+        return m_reals[key];
+    }
+
+    Vector3& BlackBoard::GetReferencedVector(const std::string& key)
+    {
+        if (m_vectors.find(key) == m_vectors.end())
+        {
+            m_vectors.emplace(key, Vector3());
+        }
+        return m_vectors[key];
+    }
+
+    Quaternion& BlackBoard::GetReferencedQuaternion(const std::string& key)
+    {
+        if (m_quaternions.find(key) == m_quaternions.end())
+        {
+            m_quaternions.emplace(key, Quaternion());
+        }
+        return m_quaternions[key];
+    }
+
+    std::string& BlackBoard::GetReferencedString(const std::string& key)
+    {
+        if (m_strings.find(key) == m_strings.end())
+        {
+            m_strings.emplace(key, "");
+        }
+        return m_strings[key];
+    }
+
+    bool BlackBoard::HasBoolean(const std::string& key)
+    {
+        return m_booleans.find(key) != m_booleans.end();
+    }
+
+    bool BlackBoard::HasInteger(const std::string& key)
+    {
+        return m_integers.find(key) != m_integers.end();
+    }
+
+    bool BlackBoard::HasPositive(const std::string& key)
+    {
+        return m_positives.find(key) != m_positives.end();
+    }
+
+    bool BlackBoard::HasReal(const std::string& key)
+    {
+        return m_reals.find(key) != m_reals.end();
+    }
+
+    bool BlackBoard::HasVector(const std::string& key)
+    {
+        return m_vectors.find(key) != m_vectors.end();
+    }
+
+    bool BlackBoard::HasQuaternion(const std::string& key)
+    {
+        return m_quaternions.find(key) != m_quaternions.end();
+    }
+
+    bool BlackBoard::HasString(const std::string& key)
+    {
+        return m_strings.find(key) != m_strings.end();
+    }
 }
--- Build/Source/System/Logic/ArtificialIntelligence/BlackBoard.hpp
+++ Build/Source/System/Logic/ArtificialIntelligence/BlackBoard.hpp
@@ -1,4 +1,9 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+#include "../../Math/Utility/MathDef.hpp"
+#include "../../Math/Algebra/Vector3.hpp"
+#include "../../Math/Algebra/Quaternion.hpp"
 
 namespace Engine5
 {
@@ -8,8 +13,48 @@ namespace Engine5
         BlackBoard();
         ~BlackBoard();
 
+        bool HasKey(const std::string& key);
+        void Clear();
+
+        void SetBoolean(const std::string& key, bool data);
+        void SetInteger(const std::string& key, int data);
+        void SetPositive(const std::string& key, size_t data);
+        void SetReal(const std::string& key, Real data);
+        void SetVector(const std::string& key, const Vector3& data);
+        void SetQuaternion(const std::string& key, const Quaternion& data);
+        void SetString(const std::string& key, const std::string& data);
+
+        bool        GetBoolean(const std::string& key);
+        int         GetInteger(const std::string& key);
+        size_t      GetPositive(const std::string& key);
+        Real        GetReal(const std::string& key);
+        Vector3     GetVector(const std::string& key);
+        Quaternion  GetQuaternion(const std::string& key);
+        std::string GetString(const std::string& key);
+
+        bool&        GetReferencedBoolean(const std::string& key);
+        int&         GetReferencedInteger(const std::string& key);
+        size_t&      GetReferencedPositive(const std::string& key);
+        Real&        GetReferencedReal(const std::string& key);
+        Vector3&     GetReferencedVector(const std::string& key);
+        Quaternion&  GetReferencedQuaternion(const std::string& key);
+        std::string& GetReferencedString(const std::string& key);
+
+        bool HasBoolean(const std::string& key);
+        bool HasInteger(const std::string& key);
+        bool HasPositive(const std::string& key);
+        bool HasReal(const std::string& key);
+        bool HasVector(const std::string& key);
+        bool HasQuaternion(const std::string& key);
+        bool HasString(const std::string& key);
+
     private:
+        std::unordered_map<std::string, bool>        m_booleans;
+        std::unordered_map<std::string, int>         m_integers;
+        std::unordered_map<std::string, size_t>      m_positives;
+        std::unordered_map<std::string, Real>        m_reals;
+        std::unordered_map<std::string, Vector3>     m_vectors;
+        std::unordered_map<std::string, Quaternion>  m_quaternions;
+        std::unordered_map<std::string, std::string> m_strings;
     };
-
-
 }
