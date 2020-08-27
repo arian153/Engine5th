a05db467f3df5756412eaa1179a7ab9ec32561f6
jangseok.han (arian153) <p084111@gmail.com>
2020-08-27 14:53:34 -0700

    Add editor command

--- Build/Source/System/GUI/Editor/Command/EditorCommand.hpp
+++ Build/Source/System/GUI/Editor/Command/EditorCommand.hpp
@@ -21,4 +21,34 @@ namespace Engine5
         std::string      m_type;
     private:
     };
+
+    template <typename T>
+    class EditData final : public EditorCommand
+    {
+    public:
+        EditData(T& data, const T& changed)
+            : m_data(data), m_prev(data), m_next(changed)
+        {
+            m_type = "Edit Data";
+        }
+
+        ~EditData()
+        {
+        }
+
+        void Execute() override
+        {
+            m_data = m_next;
+        }
+
+        void UnExecute() override
+        {
+            m_data = m_prev;
+        }
+
+    private:
+        T& m_data;
+        T  m_prev;
+        T  m_next;
+    };
 }
