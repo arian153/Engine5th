2c8dc10f9bb324b69196aa61cc85da41e2f6ad37
eric <p084111@gmail.com>
2020-09-14 00:49:18 -0700

    Update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,48 +1,43 @@
 workspace "Project-E5"
-    configurations {"Debug", "Release"}
-    warnings "Extra"
-    location ""
-    platforms { "x64" }
-    filter { "platforms:x64" }
-        system "Windows"
-        architecture "x64"
-    filter { }
+location ""
+configurations {"Debug", "Release"}
+platforms {"x64"}
+filter {"platforms:x64"}
+system "Windows"
+architecture "x64"
+filter {}
 
 project "Engine5"
-    kind "WindowedApp"
-    language "C++"
-    location ""
-    targetname ("Engine5")
-    targetdir "Bin/%{cfg.buildcfg}"
-    objdir "Obj/%{cfg.buildcfg}"
-    debugdir "Bin/%{cfg.buildcfg}"
-    systemversion "10.0"
-    flags {"MultiProcessorCompile", "NoMinimalRebuild", "NoBufferSecurityCheck"}
-    warnings "Extra"
-    includedirs {"Include"}
-    libdirs {"Lib"}
-    
+kind "WindowedApp"
+language "C++"
+location ""
+targetname("Engine5")
+targetdir "Bin/%{cfg.buildcfg}"
+objdir "Obj/%{cfg.buildcfg}"
+debugdir "Bin/%{cfg.buildcfg}"
+systemversion "10.0"
+flags {"MultiProcessorCompile", "NoMinimalRebuild", "NoBufferSecurityCheck"}
+warnings "Extra"
+includedirs {"Include"}
+links {
+    "dxgi", "d3d11", "d3dcompiler", "d2d1", "dwrite", "dsound", "dxguid",
+    "winmm", "Xinput9_1_0"
+}
+libdirs {"Lib/%{cfg.buildcfg}"}
+files {"**.h", "**.hpp", "**.cpp", "**.inl"}
+buildoptions {"-std:c++17"}
+removefiles {"**Template.cpp"}
+disablewarnings {
+    "4505", "4456", "4201", "4100", "4189", "4458", "4819", "4127", "4701",
+    "4291", "6385"
+}
+defines {"_CRT_SECURE_NO_WARNINGS", "E5_WIN32", "E5_DIRECTX11"}
 
-    files {"**.h", "**.hpp", "**.cpp", "**.inl"}
-    removefiles {"**Template.cpp"}
-
-    filter "platforms:x64"
-        system "Windows"
-        architecture "x64"
-        defines {"_CRT_SECURE_NO_WARNINGS", "E5_WIN32", "E5_DIRECTX11"}
-        buildoptions {"-std:c++17"}
-        links {"dxgi", "d3d11", "d3dcompiler", "d2d1", "dwrite", "dsound", "dxguid", "winmm", "Xinput9_1_0"}
-        disablewarnings {"4505", "4456", "4201", "4100", "4189", "4458", "4819", "4127", "4701", "4291", "6385"}
-
-    filter "configurations:Debug"
-        defines {"DEBUG"}
-        symbols "On"
-   
-
-    filter "configurations:Release"
-        defines {"NDEBUG"}
-        optimize "On"
-     
-
-     
+filter "configurations:Debug"
+defines {"DEBUG"}
+symbols "On"
+filter "configurations:Release"
+defines {"NDEBUG"}
+optimize "On"
+filter {}
 
