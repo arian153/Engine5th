cfd4a2fa304cd38afc08cb99339cc702d4057220
Eric1245 <p084111@gmail.com>
2021-05-29 21:02:58 -0700

    add basic PS code. but it need to fix

--- Resource/Shader/Phong.hlsl
+++ Resource/Shader/Phong.hlsl
@@ -52,7 +52,46 @@ VSOut VS(VSIn input)
 
 float4 PS(VSOut input)
 {
-    float4 final_color;
+    float3 normal_world = normalize(input.normal);
+    float3 to_eye_world = normalize(cam_pos - input.pos_world);
+
+    float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 specular = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+    float4 A, D, S;
+
+    for (int i = 0; i < light_count; ++i)
+    {
+        if (dynamic_light[i].type == 0)
+        {
+            CalculateAmbient(material, dynamic_light[i], ...);
+        }
+        else if (dynamic_light[i].type == 1)
+        {
+            CalculateDirectional(material, dynamic_light[i], ...);
+        }
+        else if (dynamic_light[i].type == 2)
+        {
+            CalculatePoint(material, dynamic_light[i], ...);
+        }
+        else if (dynamic_light[i].type == 3)
+        {
+            CalculateSpot(material, dynamic_light[i], ...);
+        }
+        else if (dynamic_light[i].type == 4)
+        {
+            CalculateCapsule(material, dynamic_light[i], ...);
+        }
+
+    }
+
+
+    float4 final_color = ambient + diffuse + specular;
+    // take alpha from diffuse materal
+    final_color.a = material.diffuse.a; 
+
+    //do other process.
 
     return final_color;
 }
